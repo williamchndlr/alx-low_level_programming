a50bd3530ac6620cbedda216a782e015c9daefdf
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-17 13:51:39 -0800

    s8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+char emp = '\n';
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+for (a = 97; a < 102; a++)
+{
+putchar(a);
+}
+putchar(emp);
+
+return (0);
+}
