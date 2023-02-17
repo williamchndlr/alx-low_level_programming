ff4459d5b880921082b7338484ce42aaaf4bec8f
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-17 11:03:37 -0800

    s4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,8 +14,9 @@ int main(void)
 {
 char ch;
 char emp = '\n';
-for (ch = 'a'; ch <= 'z' || ch != 'e' || ch != 'q'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
+if (ch != 'e' && ch != 'q')
 putchar(ch);
 }
 putchar(emp);
