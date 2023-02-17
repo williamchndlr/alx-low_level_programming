8ab6eaa37f35c8c5194f782554a17688cd561866
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-17 10:31:34 -0800

    s3

--- 0x01-variables_if_else_while/test.c
+++ 0x01-variables_if_else_while/test.c
@@ -4,22 +4,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-/* betty style doc for function main goes there */
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
 int main(void)
 {
 char ch;
 char emp = '\n';
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
-for (ch ='A'; ch <= 'Z'; ch++)
+for(ch='a'; ch<='z'; ch++)
 {
 putchar(ch);
 }
