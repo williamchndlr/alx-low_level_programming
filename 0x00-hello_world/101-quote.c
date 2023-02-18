ef6957641d521a0ee378ecb780b077b2e03cd760
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-18 13:11:48 -0800

    s8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  msg,"and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+    fprintf(stdout, "%s", msg);
+  fprintf(stderr, "%s", msg);
+  return (1);
+}
