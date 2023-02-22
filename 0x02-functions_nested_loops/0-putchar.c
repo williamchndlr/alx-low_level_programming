5711aac8aa1fb1421193ef94f66cd6a4598711a4
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-22 11:18:05 -0800

    s03

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,8 +9,13 @@
 
 int main (void)
 {
-_putchar("_putchar");
 
+char str[] = {'_','p','u','t','c','h','a','r','\0'};
+for (int i = 0; i <=7; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 return (0);
 
 }
