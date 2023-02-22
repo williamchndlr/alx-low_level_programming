a9b493ef5ed6ffc9c1be795858e76e39817b6d99
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-22 11:36:29 -0800

    s08

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include <unistd.h>
 #include "main.h"
 /**
- *main  _putchar writes _putchar
- *to the stdout
+ *_putchar used inside main writes _putchar to the stdout
  * Return: On success 0.
  * this program will use the method _putchar to print the word _putchar via
  * a for loop
  */
 
-int main (void)
+int main(void)
 {
 
 char str[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
 int i;
-for(i = 0; i <= 7; i++)
+for (i = 0; i <= 7; i++)
 {
 _putchar(str[i]);
 }
