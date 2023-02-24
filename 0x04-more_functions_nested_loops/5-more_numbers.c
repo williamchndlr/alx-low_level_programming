0c44e52a2cffc7f45e46b0295c8a48c37bec6137
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-24 15:02:20 -0800

    s5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *more_numbers - Entry point
+ *
+ *Return: Void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int b;
+
+	for (i = 0; i < 10; i++)
+	{
+	  for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+				_putchar(b / 10 + 48);
+			_putchar(b % 10 + 48);
+		}
+		_putchar('\n');
+		b = 0;
+	}
+}
