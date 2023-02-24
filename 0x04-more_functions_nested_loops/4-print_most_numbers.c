ea776ef4e882e52a2641799c10c3184851e958a2
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-24 14:56:57 -0800

    s4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_most_numbers - Entry point
+ *
+ *Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a != 50 && a != 52)
+			_putchar(a);
+	}
+	_putchar('\n');
+}
