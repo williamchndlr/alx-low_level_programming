65974f5a476777b8e57e4dd4bde175825ff9b618
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-24 14:48:07 -0800

    s2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *mul - Entry point
+ *@a: integer argument
+ *@b: integer argument
+ *
+ *Return: product of two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
