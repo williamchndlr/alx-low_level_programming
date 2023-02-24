67daa2e368808ed3a29ba38741d9262b1ab4b2a1
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-24 15:33:00 -0800

    s8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_square - Entry point
+ *@size: Integer argument
+ *
+ *Return: Void
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0, i < size; i++)
+	{
+	  for (j = 0; j <= sizej++)
+		{
+			if (j == size)
+				_putchar('\n');
+			else
+				_putchar('#');
+		}
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
