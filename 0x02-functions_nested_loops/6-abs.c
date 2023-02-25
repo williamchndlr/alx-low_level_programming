df96281fcaaec21ac61125df306b9b6480772e47
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-24 16:22:28 -0800

    s6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_abs - Entry point
+ *@n: Integer argument
+ *
+ *Return: always return n as a positive number
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
