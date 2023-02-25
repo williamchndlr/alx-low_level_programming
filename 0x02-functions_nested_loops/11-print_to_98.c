a92f362f1d70f36c7e855ddebb81edc343f8dbf2
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-24 17:00:39 -0800

    s11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_to_98 - Entry point
+ *@a: Integer argument
+ *
+ *Return: n
+*/
+void print_to_98(int n)
+{
+  int i;
+  for (i = n; i <= 98; i++)
+    {
+      if (i > 0)
+	{
+          printf("%d, i");
+	  printf(", ");
+        }
+    }
+  printf("\n");
+  return (0);
+  
+}
