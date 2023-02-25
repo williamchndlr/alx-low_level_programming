96d1ed274fa4d7b29dc4b8858e0b6796be45c195
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-24 16:41:21 -0800

    s10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - Entry point
+ *@a: Integer argument
+ *@b: Integer argument
+ *
+ *Return: sum - a + b
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
