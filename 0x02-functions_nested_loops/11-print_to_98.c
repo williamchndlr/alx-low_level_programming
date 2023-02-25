0814f8f9c21bfca3dbb983e240bbd62fcad04ef0
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-02-24 17:31:32 -0800

    s

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,14 +7,23 @@
 */
 void print_to_98(int n)
 {
-while (n <= 98)
+  if (n < 98)
     {
-      if (n > 0)
-	{
+      while (n <= 98)
+        {
           printf("%d", n);
-	  printf(", ");
+          printf(", ");
         }
-      n++;
+        n++;
     }
+  else if (n > 98)
+    {
+      while (n >= 98)
+	{
+	  printf("%d", n);
+          printf(", ");
+	}
+        n--;
+    }	  
   printf("\n");  
 }
