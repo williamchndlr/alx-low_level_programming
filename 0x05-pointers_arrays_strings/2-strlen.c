71ee35fd34add00be2214bd5e67b5d865cdf1176
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-03-01 11:17:37 -0800

    s2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - Entry point
+ * @s: Takes a pointer to an int as argument
+ *
+ * Return: The length of s
+ */
+
+int _strlen(char *s);
+{
+int count = 0;
+while (s != \0)
+{
+count++;
+}
+return (count);
+}
