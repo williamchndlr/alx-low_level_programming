31b6d8103ceaf18d940c4622d165acfe77d1f6b4
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-03-01 11:33:16 -0800

    s4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @str: Takes a pointer to a char as argument
+ *
+ * Return: Void
+ */
+
+void _puts(char *str)
+{
+  _putchar(str);
+  _putchar('\n');y
+}
