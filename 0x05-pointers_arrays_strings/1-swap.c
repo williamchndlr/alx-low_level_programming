e80dfbe8a5fb3f4a5392c82268490573495435d4
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-03-01 10:44:37 -0800

    s1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ * @a: Takes a pointer to an int as argument
+ * @b: Takes a pointer to an int as argument
+ *
+ * Return: Void
+ */
+
+void swap_int(int *a, int *b)
+{
+  int c = *a;
+  *a = *b;
+  *b =c;
+}
