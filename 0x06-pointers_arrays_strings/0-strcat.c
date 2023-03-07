c349c250293d2f56ad3392b56e039fb28cf53148
williamchndlr <60436308+williamchndlr@users.noreply.github.com>
2023-03-07 12:22:59 -0800

    s0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,17 @@
 char *_strcat(char *dest, char *src)
 {
 int d = 0, int s = 0;
-while (dest[d] != "\0")
+while (dest[d] != '\0')
 {
 d++;
 }
-while (src[s] != "\0")
+while (src[s] != '\0')
 {
 dest[d] = src[s];
 s++;
 d++;
 }
-dest[d] = "\0";
+dest[d] = '\0';
   
 return (dest);
 }
